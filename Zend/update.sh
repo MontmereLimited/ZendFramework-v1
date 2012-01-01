fe9b37bdaa646ff94772b01c6852104f6a948204
Stephen J. Fuhry <steve@montmere.com>
2012-01-01 14:26:03 -0500

    update for 2012-01-01 14:25:51

---
+++ Zend/update.sh
@@ -0,0 +1,27 @@
+#http://framework.zend.com/svn/framework/standard/branches/release-1.11/library/Zend
+
+# current directory path
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+TODAY="$( date +"%Y-%m-%d %H:%M:%S" )"
+
+# update zend
+svn update "${DIR}/../Zend"
+svn update "${DIR}/../ZendX"
+
+# copy to staging location without .svn directories
+rsync -avC "${DIR}/../Zend" "${DIR}"
+rsync -avC "${DIR}/../ZendX" "${DIR}"
+
+# strip require_once since we always use an autoloader
+find "${DIR}/Zend" -name '*.php' -not -wholename '*/Loader/Autoloader.php' \
+	-not -wholename '*/Application.php' -print0 | \
+	xargs -0 sed --regexp-extended --in-place 's/(require_once)/\/\/ \1/g'
+
+find "${DIR}/ZendX" -name '*.php' -not -wholename '*/Loader/Autoloader.php' \
+	-not -wholename '*/Application.php' -print0 | \
+	xargs -0 sed --regexp-extended --in-place 's/(require_once)/\/\/ \1/g'
+
+# push to git
+git add .
+git commit -m "update for ${TODAY}"
+git push origin master
